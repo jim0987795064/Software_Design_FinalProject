d580ea84d73ae41d92d03127e2016756deedaaf2
jim19970709 <jim0987795064@gmail.com>
2022-01-01 17:53:58 +0800

    First commit

--- GUI.java
+++ GUI.java
@@ -498,6 +498,67 @@ public class GUI {
  		}
  		textLabel6.setText(s);
     }
+	public void HAVING_Press(ActionEvent e)throws SQLException{
+    	Statement statement = connection.createStatement();
+    	String query1 = in1.getText();
+    	String query2 = in2.getText();
+    	String query3 = in3.getText();
+    	String query4 = in4.getText();
+    	String query5 = in5.getText();
+    	String query6 = in6.getText();
+    	String query ="SELECT "+query1+" FROM "+query2+" WHERE "+query3+" GROUP BY "+query4+" HAVING "+query5+" ORDER BY "+query6+";";
+    	int replace = 0;
+ 		String s = "";
+ 		ResultSet Select_Result = statement.executeQuery(query);
+ 		while(Select_Result.next()) {
+ 			if(replace == 0) {
+ 				s += "Student_Number\n";
+ 				replace++;
+ 			}
+ 			s += Select_Result.getString("Student_Number") + "\n";
+ 		}
+ 		textLabel6.setText(s);
+    }
+
+	public void BETWEEN_Press(ActionEvent e)throws SQLException{
+    	Statement statement = connection.createStatement();
+    	String query1 = in1.getText();
+    	String query2 = in2.getText();
+    	String query3 = in3.getText();
+    	String query4 = in4.getText();
+    	String query5 = in5.getText();
+    	String query ="SELECT "+query1+" FROM "+query2+" WHERE "+query3+" BETWEEN "+query4+" AND "+query5";";
+    	int replace = 0;
+ 		String s = "";
+ 		ResultSet Select_Result = statement.executeQuery(query);
+ 		while(Select_Result.next()) {
+ 			if(replace == 0) {
+ 				s += "Student_Number\n";
+ 				replace++;
+ 			}
+ 			s += Select_Result.getString("Student_Number") + "\n";
+ 		}
+ 		textLabel6.setText(s);
+    }
+
+	public void Aliases_Press(ActionEvent e)throws SQLException{
+    	Statement statement = connection.createStatement();
+    	String query1 = in1.getText();
+    	String query2 = in2.getText();
+    	String query3 = in3.getText();
+    	String query ="SELECT "+query1+" AS "+query2+" FROM "+query3+";";
+    	int replace = 0;
+ 		String s = "";
+ 		ResultSet Select_Result = statement.executeQuery(query);
+ 		while(Select_Result.next()) {
+ 			if(replace == 0) {
+ 				s += "Student_Number\n";
+ 				replace++;
+ 			}
+ 			s += Select_Result.getString("Student_Number") + "\n";
+ 		}
+ 		textLabel6.setText(s);
+    }
      
      //for query
 	public void Query_Press(ActionEvent e) throws SQLException{
@@ -586,9 +647,47 @@ public class GUI {
      		textLabel6.setText(s);
      	}
 
-     	/*else if(sql.contains("HAVING")) {
-     		
-     	}*/
+     	else if(sql.contains("HAVING")) {
+     		int replace = 0;
+     		String s = "";
+     		ResultSet Select_Result = statement.executeQuery(sql);
+     		while(Select_Result.next()) {
+     			if(replace == 0) {
+     				s += "Student_Number\n";
+     				replace++;
+     			}
+     			s += Select_Result.getString("Student_Number") + "\n";
+     		}
+     		textLabel6.setText(s);
+     	}
+
+		else if(sql.contains("BETWEEN")) {
+     		int replace = 0;
+     		String s = "";
+     		ResultSet Select_Result = statement.executeQuery(sql);
+     		while(Select_Result.next()) {
+     			if(replace == 0) {
+     				s += "Student_Number\n";
+     				replace++;
+     			}
+     			s += Select_Result.getString("Student_Number") + "\n";
+     		}
+     		textLabel6.setText(s);
+     	}
+		
+		else if(sql.contains("ALIASES")) {
+     		int replace = 0;
+     		String s = "";
+     		ResultSet Select_Result = statement.executeQuery(sql);
+     		while(Select_Result.next()) {
+     			if(replace == 0) {
+     				s += "Student_Number\n";
+     				replace++;
+     			}
+     			s += Select_Result.getString("Student_Number") + "\n";
+     		}
+     		textLabel6.setText(s);
+     	}
 
      	 else if(sql.substring(0,1).equals("S")) {
      		 System.out.println("Select");
@@ -852,24 +951,17 @@ public class GUI {
 
      			}
      		}//
-     		
      	 }
      	 else {
      		 System.out.println("other");
-      		//statement.executeUpdate(sql);
-      		
+      		//statement.executeUpdate(sql);		
      	 }
      	textLabel1.setText(table);
      	textLabel2.setText(table2);
      	textLabel3.setText(table3);
      	textLabel4.setText(table4);
-     } catch (SQLException e1) {
-    	 
+     } catch (SQLException e1) {	 
          e1.printStackTrace();
      }
-
-	
 	}
-	
-	
 }
